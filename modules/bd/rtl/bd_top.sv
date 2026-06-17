376c44342ffc779b9d39a2dd6b946365fdff2ee2
RDSik <dmitryryabickov@yandex.ru>
2026-06-17 13:00:51 +0300

    fix interface

--- modules/bd/rtl/bd_top.sv
+++ modules/bd/rtl/bd_top.sv
@@ -54,7 +54,7 @@ module bd_top (
     axil_if.master s2mm_axil,
     axil_if.master mm2s_axil,
     axil_if.master ext_axil,
-    axi_if.master  hp_axi
+    axi_if.slave   hp_axi
 );
 
     libre_bd i_libre_bd (
--- modules/common/rtl/axi_to_axil.sv
+++ modules/common/rtl/axi_to_axil.sv
@@ -1,30 +1,30 @@
 module axi_to_axil (
-	axi_if.slave   s_axi,
-	axil_if.master m_axil
+    axi_if.slave   s_axi,
+    axil_if.master m_axil
 );
 
-	assign m_axil.awaddr  = s_axi.awaddr;
-	assign m_axil.awprot  = s_axi.awprot;
-	assign m_axil.awvalid = s_axi.awvalid;
-	assign s_axi.awready  = m_axil.awready;
+    assign m_axil.awaddr = s_axi.awaddr;
+    assign m_axil.awprot = s_axi.awprot;
+    assign m_axil.awvalid = s_axi.awvalid;
+    assign s_axi.awready = m_axil.awready;
 
-	assign m_axil.wdata  = s_axi.wdata;
-	assign m_axil.wstrb  = s_axi.wstrb;
-	assign m_axil.wvalid = s_axi.wvalid;
-	assign s_axi.wready  = m_axil.wready;
+    assign m_axil.wdata = s_axi.wdata;
+    assign m_axil.wstrb = s_axi.wstrb;
+    assign m_axil.wvalid = s_axi.wvalid;
+    assign s_axi.wready = m_axil.wready;
 
-	assign s_axi.bresp   = m_axil.bresp;
-	assign s_axi.bvalid  = m_axil.bvalid;
-	assign m_axil.bready = s_axi.bready;
+    assign s_axi.bresp = m_axil.bresp;
+    assign s_axi.bvalid = m_axil.bvalid;
+    assign m_axil.bready = s_axi.bready;
 
-	assign m_axil.araddr  = s_axi.araddr;
-	assign m_axil.arprot  = s_axi.arprot;
-	assign m_axil.arvalid = s_axi.arvalid;
-	assign s_axi.arready  = m_axil.arready;
+    assign m_axil.araddr = s_axi.araddr;
+    assign m_axil.arprot = s_axi.arprot;
+    assign m_axil.arvalid = s_axi.arvalid;
+    assign s_axi.arready = m_axil.arready;
 
-	assign s_axi.rdata   = m_axil.rdata;
-	assign s_axi.rresp   = m_axil.rresp;
-	assign s_axi.rvalid  = m_axil.rvalid;
-	assign m_axil.rready = s_axi.rready;
+    assign s_axi.rdata = m_axil.rdata;
+    assign s_axi.rresp = m_axil.rresp;
+    assign s_axi.rvalid = m_axil.rvalid;
+    assign m_axil.rready = s_axi.rready;
 
 endmodule
